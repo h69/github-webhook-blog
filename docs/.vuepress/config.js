b7d2501b6d73e3dddc5ed42d4a1ec65f7b7c438d
h69 <517364792@qq.com>
2020-12-24 18:58:27 +0800

    add baidu seo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,4 +10,8 @@ module.exports = {
     sidebar: {},
   },
   plugins: ["@vuepress/back-to-top"],
+  head: [
+    ["meta", { name: "baidu-site-verification", content: "code-FacPXl4enQ" }],
+    ["script", { type: "text/javascript", src: "/assets/js/push.js" }],
+  ],
 };
